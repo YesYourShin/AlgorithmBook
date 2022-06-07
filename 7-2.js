7ea0b623449dd7e2bdade37f6403c22e99ccdf70
YesYourShin <ehrl827@naver.com>
2022-06-07 15:00:36 +0900

    7-2 수정

--- 7-2.js
+++ 7-2.js
@@ -1,12 +1,9 @@
-var BigNumber = require('big-number');
-const n = 113;
-const rn = [];
-const rnRemainder = [];
+const fermatTest = n => {
+    const BigNumber = require('big-number'); // 큰 수의 정확한 계산을 위한 라이브러리
+    const rd = Math.floor(Math.random() * n); // 0 <= random < 113
+    // n보다 작은 값을 mod 연산한 값
+    const remainder = BigNumber(rd).pow(n).mod(n);
+    return console.log(n + '보다 작은 랜덤 값:', rd, ', mod 연산 값:', remainder.toString());
+};
 
-for (let i = 0; i < 3; i++) {
-    rn[i] = Math.floor(Math.random() * n);
-    rnRemainder[i] = BigNumber(rn[i]).pow(n).mod(n);
-}
-
-console.log(rn);
-console.log(rnRemainder.toString());
+fermatTest(561); // 숫자 입력
