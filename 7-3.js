29dff9b22c951fff2c9be3975eaa814b8f420d30
YesYourShin <ehrl827@naver.com>
2022-06-07 20:23:56 +0900

    add comment

--- 7-3.js
+++ 7-3.js
@@ -1,40 +1,47 @@
-const teleportProbability = 0.15;
-const linkProbability = 1 - teleportProbability;
-
-let page = [0, 0, 0, 0];
+// 등록된 페이지
+const page = [0, 0, 0, 0];
 
 const simulation = () => {
+    // 처음 시작할 페이지
     const startPage = Math.floor(Math.random() * 4);
-
-    start(startPage);
+    return start(startPage);
 };
 
 const start = startPage => {
+    // 입장한 페이지 스코어 업
     page[startPage] += 1;
     if (startPage === 3) {
-        loop(1);
+        // 루프할 수 있도록 다른 페이지 연결
+        return loop(1);
     } else {
-        loop(startPage + 1);
+        // 다음 페이지로
+        return loop(startPage + 1);
     }
 };
 
 const loop = startPage => {
+    // 입장한 페이지 스코어 업
     page[startPage] += 1;
+    // 1000회 시뮬을 위한 페이지 스코어 합산 확인
     let sum = 0;
     for (let times of page) {
         sum = sum + times;
     }
     if (sum === 1000) {
+        // 1000회일 경우 스톱
         return;
     } else {
         const probability = Math.random();
         if (probability <= 0.15) {
-            simulation();
+            // 15% 확률로 텔레포트
+            return simulation();
         } else {
             if (startPage === 3) {
-                loop(1);
+                // 루프할 수 있도록 다른 페이지 연결
+                return loop(1);
             } else {
-                loop(startPage + 1);
+                // 다음 페이지로
+                return loop(startPage + 1);
             }
         }
     }
@@ -42,6 +49,8 @@ const loop = startPage => {
 
 simulation();
 
+// 비율로 바꾸기
 const percentage = page.map(times => Math.round((times * 100) / 1000));
+
 console.log(page);
 console.log(percentage);
--- 7-4.js
+++ 7-4.js
@@ -5,16 +5,15 @@ const move = (n, start, to) => {
 
 const hanoi = (n, start, to, index) => {
     if (n === 1) {
-        return move(n, start, index);
+        return move(n, start, index); // 1번 원반일 경우 옮기기
     } else {
-        hanoi(n - 1, start, index, to);
-        move(n, start, index);
-        return hanoi(n - 1, to, start, index);
+        hanoi(n - 1, start, index, to); // 2번 이상의 원반의 경우 -1 재귀
+        move(n, start, index); // 작은 원반이 옮겨진 후 현재 원반 옮기기
+        return hanoi(n - 1, to, start, index); // 작은 원반을 위에 얹기
     }
 };
 
-const disc = 4;
 let times = 0;
 
-hanoi(disc, 'A', 'B', 'C');
-console.log(times);
+hanoi(2, 'A', 'B', 'C');
+console.log(times); // 원반이 옮겨진 횟수
